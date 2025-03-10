2e1463b66a848f365869a4310f509770e79b300c
twentyfour-1 <justas.justas@gmail.com>
2025-03-10 21:35:33 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hello, Legend!")
+
+name = "Legend"
+age = 100  # Integer
+height = 182  # Float
+is_cool = True  # Boolean
+
+print(name, age, height, is_cool)
+
+a = 10
+b = 3
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division (float)
+print(a // b)  # Integer Division
+print(a % b)  # Modulus (remainder)
+print(a ** b)  # Exponentiation (10^3)
